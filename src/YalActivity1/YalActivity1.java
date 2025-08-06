58952c71aa81894c179b374b5b075380f4c0fdcb
CarlosYalung <yalungcarlos17@gmail.com>
2025-08-06 18:27:56 +0800

    Activities

--- src/YalActivity1/YalActivity1.java
+++ src/YalActivity1/YalActivity1.java
@@ -1,9 +1,9 @@
 
-package carlosyalung2c;
+package YalActivity1;
 
 import java.util.Scanner;
 
-public class CARLOSYALUNG2C {
+public class YalActivity1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
